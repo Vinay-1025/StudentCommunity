3f0eda3cecd0d9474ab9bbf0b2d4398fb07d89cf
Vinay-1025 <darkeaglerules6ios@gmail.com>
2023-10-26 15:21:03 +0530

    uploading the navigation tabs

---
+++ src/screens/HomeScreen/Community.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Community = () => {
+    return (
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Community</Text>
+        </View>
+    )
+}
+
+export default Community
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,23 +1,85 @@
-import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+import Home from './Home';
+import Post from './Post';
+import Upload from './Upload';
+import Community from './Community';
+import Settings from './Settings';
 
-const HomeScreen = () => {
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    const scaleValue = new Animated.Value(1);
+
+    const tabBarOptions = {
+        activeTintColor: 'black',
+        inactiveTintColor: '#999',
+        style: { backgroundColor: '#5522', height: 70 }, // Increase tab bar height
+    };
+
     return (
-        <View style={styles.title}>
-            <Text style={{fontSize:30, alignSelf:'center', color :'red'}}>Welcome to STUDENT COMMUNITY</Text>
-        </View>
-    )
-}
+        <View style={styles.root}>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'home' : 'home-outline';
+                        } else if (route.name === 'Post') {
+                            iconName = focused ? 'information-circle' : 'information-circle-outline';
+                        } else if (route.name === 'Upload') {
+                            iconName = focused ? 'add-circle' : 'add-circle-outline';
+                        } else if (route.name === 'Community') {
+                            iconName = focused ? 'people' : 'people-outline';
+                        } else if (route.name === 'Settings') {
+                            iconName = focused ? 'settings' : 'settings-outline';
+                        }
 
-const styles = StyleSheet.create ({
-    title:{
-        top: '50%',
-        color: 'red',
-        fontSize: 30,
-        justifyContent: 'center',
-        alignItems: 'center',
+                        return (
+                            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                                <Animated.View
+                                    style={[
+                                        {
+                                            transform: [
+                                                {
+                                                    scale: focused
+                                                        ? scaleValue.interpolate({
+                                                            inputRange: [0, 2],
+                                                            outputRange: [1, 1.5],
+                                                        })
+                                                        : 1,
+                                                },
+                                            ],
+                                        },
+                                    ]}
+                                >
+                                    <Ionicons name={iconName} size={size} color={color} />
+                                </Animated.View>
+                            </View>
+                        );
+                    },
+                })}
+                tabBarOptions={tabBarOptions}
+            >
+                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="Post" component={Post} />
+                <Tab.Screen name="Upload" component={Upload} />
+                <Tab.Screen name="Community" component={Community} />
+                <Tab.Screen name="Settings" component={Settings} />
+            </Tab.Navigator>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: '#000',
     },
 });
 
-export default HomeScreen
+export default App;
